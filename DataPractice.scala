f0727546d46af039a942d20b18954e589fb08223
JinsuKIM <jinsu6688@gmail.com>
2020-03-05 17:10:32 +0900

    I practiced writting data type

---
+++ DataPractice.scala
@@ -0,0 +1,16 @@
+//암시적 선언
+var x = 10
+var y =''abc'
+
+// 명시적 선언
+var b : byte = 10 // var 변수이름 옆에 : byte or Int or ~~을 적어준다.
+var s : Short = 10
+var i : Int = 10
+
+// 값에 약어를 추가하여 명시적 선언
+var f = 10.0f
+var d = 20.0d
+
+// 암시적인 선언, 컴파일러가 자동으로 타입을 선택
+ii: Int = 10
+scala> var ii = 10
